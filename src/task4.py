97e62ea0a6c1ac384951b83fc8ce884a4d87ef7d
ArsRoz <85549337+ArsRoz@users.noreply.github.com>
2021-06-23 12:36:16 +0300

    Update task4.py

--- src/task4.py
+++ src/task4.py
@@ -1,12 +1,12 @@
-w= list(input("Строка: "))
+w = list(input("Строка: "))
 a = 0
 b = 0
 english = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 for i in w:
     if i in english:
-        if i.isupper() == True:
+        if i.isupper():
             a += 1
-        elif i.islower() == True:
+        elif i.islower():
             b += 1
-print(a ," Строчных")
-print(b ,"Прописных")
+print(a, " Строчных")
+print(b, "Прописных")
