2b18bb17bcf37caf276c8e6e84382cc01945a1fa
ArsRoz <85549337+ArsRoz@users.noreply.github.com>
2021-06-23 12:35:29 +0300

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -1,7 +1,7 @@
-string = list(input("Строка  : "))
+string = list(input("Строка: "))
 string_1 = []
 for i in string:
     if i not in string_1:
         if i != " ":
             string_1.append(i)
-print ("".join(string_1)) 
+print("".join(string_1))
