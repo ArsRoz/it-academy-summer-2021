278faf12e6ed16a343029e4ce52e113c16945277
ArsRoz <85549337+ArsRoz@users.noreply.github.com>
2021-06-23 12:40:20 +0300

    Update task1.py

--- src/task1.py
+++ src/task1.py
@@ -1,7 +1,7 @@
 m = int(input('Цена рубли  :'))
 n = int(input('Цена копеек  :'))
 s = int(input('Количество товара  :'))
-nn = int(n*s) % 100
-q = int((n*s)//100)
-rub = int((m*s+q))
+nn = int(n * s) % 100
+q = int((n * s) // 100)
+rub = int((m * s + q))
 print('Стоимость товара составляет:', rub, 'рублей',  nn, 'копеек')
