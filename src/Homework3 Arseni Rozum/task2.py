3a830e02667a30ae018c2fffc432cb751f014997
ArsRoz <85549337+ArsRoz@users.noreply.github.com>
2021-06-24 16:37:40 +0300

    со
    выва

---
+++ src/Homework3 Arseni Rozum/task2.py
@@ -0,0 +1,10 @@
+# 2.Дан список стран и городов каждой страны.
+# Затем даны названия городов.
+# Для каждого города укажите, в какой стране он находится.
+
+dict_1 = {}
+for _ in range(int(input())):
+    country, *town = input().split()
+    for town in towns:
+        dict_1[town] = country
+print(*(dict_1[input()] for _ in range(int(input()))), sep="\n")
