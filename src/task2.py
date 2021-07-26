a54ddcc414612b91a281ca6e6dc26883a61d4c5b
ArsRoz <85549337+ArsRoz@users.noreply.github.com>
2021-07-26 12:45:22 +0300

    Update task2.py

--- src/task2.py
+++ src/task2.py
@@ -1,24 +1,44 @@
-"""Создайте декоратор, который хранит результаты вызовов функции
-(за все время вызовов, не только текущий запуск программы)"""
+"""
+Создайте декоратор, который хранит результаты вызовов функции
+(за все время вызовов, не только текущий запуск программы).
+"""
 
 
 def decorator(func_new):
-    results = []
 
     def wrapper(*args, **kwargs):
-        nonlocal results
-        func_new(*args, **kwargs)
-        results.append(func_new(*args, **kwargs))
+        results = func_new(*args, **kwargs)
+        with open("results.txt", "a") as res:
+            res.write(str(results) + "\n")
         return results
 
     return wrapper
 
 
 @decorator
-def func_addition(x, y):
-    return x + y
+def func_addition(a, b):
+    return a + b
 
 
-print(func_addition(2, 3))
+print(func_addition("s", "f"))
 print(func_addition(3, 5))
 print(func_addition(5, 5))
+
+
+@decorator
+def triangle(a, b, c):
+
+    if (a + b) > c and (b + c) > a and (c + a) > b:
+        p = (a + b + c) / 2
+        square = p * (p - a) * (p - b) * (p - c)
+        square = square ** 0.5
+        square = str(square)
+        print("Это треугольник площадью " + square[:5])
+    else:
+        print("Это НЕ треугольник")
+
+
+print(triangle(5, 5, 5))
+print(triangle(10, 3, 3))
+print(triangle(3, 4, 5))
+print(triangle(5, 5, 5))
